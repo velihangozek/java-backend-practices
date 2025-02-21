70a481741a5515b4e8f2d622dd89294fc7fb20a2
Velihan Gözek <velihangozek@hotmail.com>
2025-02-21 03:24:33 +0300

    add rangelooper example

---
+++ Pre-Work-1/For-Loop-Exercises/RangeLooper/src/main/java/org/velihangozek/Main.java
@@ -0,0 +1,19 @@
+package org.velihangozek;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 101; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("\n--------------\n");
+
+        for (int i = 100; i >= 50; i--) {
+            System.out.println(i);
+        }
+
+        }
+    }
