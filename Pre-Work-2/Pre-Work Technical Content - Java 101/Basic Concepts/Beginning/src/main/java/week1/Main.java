e5e6ae0c2d093651626c096843c72cd86557eaf1
Velihan Gözek <velihangozek@hotmail.com>
2025-02-28 03:47:45 +0300

    some data types trials

---
+++ Pre-Work-2/Pre-Work Technical Content - Java 101/Basic Concepts/Beginning/src/main/java/week1/Main.java
@@ -0,0 +1,60 @@
+package week1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello \rvelihan1!\n");
+
+        System.out.println("H\'ello\b velihan\"2!");
+
+        System.out.println("\tHello v\\elihan3!\f");
+
+        int i = 35;
+
+        int i1 = 56;
+
+        boolean isA;
+        isA = true;
+
+        isA = false;
+
+        i1 = -67;
+
+        i = 200 + i1;
+
+        byte b = 127;
+        b = 127 - 255;
+
+        char c = 'c';
+        char c1 = 'A';
+
+        System.out.println(c1);
+
+        int i2 = 2_000_000_000;
+
+        long l;
+        l = 100;
+
+        //short 2m;
+        //2m = 1;
+
+        short m2;
+        m2 = 1;
+
+        int number;
+
+        int j1=0,j2=1,j3=2;
+        int k1,k2,k3;
+        k1=2;k2=4;k3=6;
+
+        // int j1=7;
+
+        boolean bool = false;
+        bool = true;
+
+        System.out.println(bool);
+
+        System.out.println(k1 + k2 + k3);
+
+    }
+}
