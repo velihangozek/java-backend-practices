0d193d5b90c279c6bce27c65b848742d0c4cbfb7
Velihan Gözek <velihangozek@hotmail.com>
2025-03-02 04:53:21 +0300

    add some more content on operators

--- Pre-Work-2/Pre-Work Technical Content - Java 101/Basic Concepts/Beginning/src/main/java/week1/Main.java
+++ Pre-Work-2/Pre-Work Technical Content - Java 101/Basic Concepts/Beginning/src/main/java/week1/Main.java
@@ -1,7 +1,12 @@
 package week1;
 
 public class Main {
+
+    static boolean bool1;
+
     public static void main(String[] args) throws InterruptedException {
+
+        System.out.println(bool1);
 
         System.out.println("Hello \rvelihan1!\n");
 
@@ -10,11 +15,11 @@ public class Main {
         System.out.println("\tHello v\\elihan3!\f");
 
         System.out.print("Loading 10%\r");
-        Thread.sleep(1000);
+        // Thread.sleep(1000);
         System.out.print("Loading 20%\r");
-        Thread.sleep(1000);
+        // Thread.sleep(1000);
         System.out.print("Loading 30%\r");
-        Thread.sleep(1000);
+        // Thread.sleep(1000);
         System.out.print("Loading 40%\n");
 
         int i = 35;
@@ -63,6 +68,65 @@ public class Main {
         System.out.println(bool);
 
         System.out.println(k1 + k2 + k3);
+
+        // Operators
+        // Arithmetic Operators
+
+        int sum = 100 + 200;
+
+        // int x = 5, y = 3;
+
+        int x = 7;
+        int y = 4;
+
+        int sum1 = x + y;
+        System.out.println("Addition: " + sum1); // String Concatenation
+
+        int result = x - y;
+        System.out.println("Subtraction: " + result);
+
+        int result1 = x * y;
+        System.out.println("Multiplication: " + result1);
+
+        int result2 = x / y;
+        System.out.println("Division: " + result2); // result in int so it will be rounded down.
+
+        int result3 = x % y;
+        System.out.println("Mod: " + result3);
+
+        //int result4 = x * 3.4;
+        double result4 = x * 3.4;
+        System.out.println(result4);
+
+        // Assignment Operator
+
+        boolean isEqual = x == y;
+        boolean isEqual1 = 4 == y;
+        System.out.println("x:" + x + " y:" + y + " Equal? " + isEqual);
+        System.out.println("x:" + 4 + " y:" + y + " Equal? " + isEqual1);
+
+        boolean notEqual = x != y;
+        boolean notEqual1 = 4 != y;
+        System.out.println("x:" + x + " y:" + y + " Not Equal? " + notEqual);
+        System.out.println("x:" + 4 + " y:" + y + " Not Equal? " + notEqual1);
+
+        boolean isGreater = x > y;
+        boolean isLesser = x < y;
+        System.out.println("x:" + x + " y:" + y + " Greater Than? " + isGreater);
+        System.out.println("x:" + x + " y:" + y + " Less Than? " + isLesser);
+
+        boolean isLessThanOrEqual = x <= y;
+        boolean isGreaterThanOrEqual = x >= y;
+        System.out.println("x:" + x + " y:" + y + " Less Than Or Equal? " + isLessThanOrEqual);
+        System.out.println("x:" + x + " y:" + y + " Greater Than Or Equal? " + isGreaterThanOrEqual);
+
+        // Logical Operators
+
+        boolean isTrue = isEqual && isEqual1;
+        boolean isTrue1 = true && false;
+        boolean isFalse = false || true;
+        boolean isFalse1 = ( false || true ) && ( false && false ) || ( true && true && false) ;
+
 
     }
 }
