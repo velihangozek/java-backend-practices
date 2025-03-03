a86e1c97e74d32fa0f7f16a3384d5be60e015999
Velihan Gözek <velihangozek@hotmail.com>
2025-03-03 04:53:58 +0300

    var in java

---
+++ Pre-Work-2/Pre-Work Technical Content - Java 101/Basic Concepts/Variable Definition with Var/VariableDeclarationWithVar/src/main/java/org/velihangozek/VarMain.java
@@ -0,0 +1,26 @@
+package org.velihangozek;
+
+public class VarMain {
+    public static void main(String[] args) {
+
+        int var = 193;
+
+        // String int = "Java"; // Keyword ile değişken isimlendirilemez.
+        // String void = "Java"; // Keyword ile değişken isimlendirilemez.
+        // String static = "Java"; // Keyword ile değişken isimlendirilemez.
+
+        // var value; // var ile declaration yapamıyoruz initialization olmadan. İlk değer ataması yok.
+
+
+
+        var value = 12;
+
+        // value = "velihan2"; // Compile Time Error. Çünkü daha önceden int olduğunu belirtmiştik.
+
+        var name = "velihan";
+        name = "velihan3";
+
+        var age = 27;
+
+    }
+}
