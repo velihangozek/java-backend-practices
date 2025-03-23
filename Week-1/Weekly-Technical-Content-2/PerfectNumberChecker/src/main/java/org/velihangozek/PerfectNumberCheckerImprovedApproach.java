5b194065c0a683b4db0bd7a0532bc57f85172094
Velihan Gözek <velihangozek@hotmail.com>
2025-03-23 18:48:35 +0300

    add perfect number checker

---
+++ Week-1/Weekly-Technical-Content-2/PerfectNumberChecker/src/main/java/org/velihangozek/PerfectNumberCheckerImprovedApproach.java
@@ -0,0 +1,38 @@
+package org.velihangozek;
+
+import java.util.Scanner;
+
+public class PerfectNumberCheckerImprovedApproach {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Please enter a positive integer to determine if it is a perfect number: ");
+
+            if (!scanner.hasNextInt()) {
+                System.out.println("Please enter a valid positive integer!!!");
+                return;
+            }
+
+            int number = scanner.nextInt();
+            if (number <= 0) {
+                System.out.println("Please enter a positive integer!!!");
+                return;
+            }
+
+            System.out.printf("%d %s%n", number, isPerfect(number) ? "is Perfect!" : "is NOT Perfect!");
+        }
+    }
+
+    private static boolean isPerfect(int number) {
+        if (number <= 1) return false;
+
+        int sum = 1;
+        int limit = (int) Math.sqrt(number);
+
+        for (int i = 2; i <= limit; i++) {
+            if (number % i == 0) {
+                sum += i + (i != number / i ? number / i : 0);
+            }
+        }
+        return sum == number;
+    }
+}
---
+++ Week-1/Weekly-Technical-Content-2/PerfectNumberChecker/src/main/java/org/velihangozek/PerfectNumberCheckerInitialApproach.java
@@ -0,0 +1,34 @@
+package org.velihangozek;
+
+import java.util.Scanner;
+
+public class PerfectNumberCheckerInitialApproach {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Please enter a positive integer to determine if it is a perfect number: ");
+
+            int number = scanner.nextInt();
+
+            if (number <= 0) {
+                System.out.println("Number must be positive!");
+                return;
+            }
+
+            int sumOfMultiples = 0;
+            for (int i = 1; i < number; i++) {
+                if (number % i == 0) {
+                    sumOfMultiples += i;
+                }
+            }
+
+            boolean isPerfect = false;
+
+            if (sumOfMultiples == number) {
+                isPerfect = true;
+                System.out.println(number + " is a PERFECT number!");
+            } else {
+                System.out.println(number + " is NOT a perfect number :(");
+            }
+        }
+    }
+}
