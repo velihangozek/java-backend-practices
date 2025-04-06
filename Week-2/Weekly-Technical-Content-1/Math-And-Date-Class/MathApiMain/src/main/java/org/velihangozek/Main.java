fc530fd824ca6b2b97fb993a2e9e5f0451e749aa
Velihan Gözek <velihangozek@hotmail.com>
2025-04-06 23:05:56 +0300

    add math api practices

---
+++ Week-2/Weekly-Technical-Content-1/Math-And-Date-Class/MathApiMain/src/main/java/org/velihangozek/Main.java
@@ -0,0 +1,78 @@
+package org.velihangozek;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int min = Math.min(4,9);
+        System.out.println(min);
+
+        int max = Math.max(28, 29);
+        System.out.println(max);
+
+        double pi = Math.PI;
+        System.out.println(pi);
+
+        double round1 = Math.round(123.50);
+        System.out.println(round1);
+
+        double round2 = Math.round(123.49);
+        System.out.println(round2);
+
+        double power = Math.pow(3, 4);
+        System.out.println(power);
+
+        double random = Math.random();
+        System.out.println(random);
+
+        double absolute = Math.abs(-95.3);
+        System.out.println(absolute);
+
+        double squareRoot = Math.sqrt(25);
+        System.out.println(squareRoot);
+
+        double cubicRoot = Math.cbrt(125);
+        System.out.println(cubicRoot);
+
+        double ceiling = Math.ceil(3.1);
+        System.out.println(ceiling);
+
+        double floor = Math.floor(4.9);
+        System.out.println(floor);
+
+        double sine = Math.sin(100);
+        System.out.println(sine);
+
+        double cosine = Math.cos(0);
+        System.out.println(cosine);
+
+        double tangent = Math.tan(0);
+        System.out.println(tangent);
+
+        double toRadians = Math.toRadians(180);
+        System.out.println(toRadians);
+
+        double toDegrees = Math.toDegrees(Math.PI);
+        System.out.println(toDegrees);
+
+        double exp = Math.exp(1);
+        System.out.println(exp);
+
+        double log = Math.log(10);
+        System.out.println(log);
+
+        double log10 = Math.log10(100);
+        System.out.println(log10);
+
+        double e = Math.E;
+        System.out.println(e);
+
+        double floorMod = Math.floorMod(10, 3);
+        System.out.println(floorMod);
+
+        double ceilMod = Math.ceilMod(10, 7);
+        System.out.println(ceilMod);
+
+        double tripleERemainder = Math.IEEEremainder(13, 9);
+        System.out.println(tripleERemainder);
+    }
+}
