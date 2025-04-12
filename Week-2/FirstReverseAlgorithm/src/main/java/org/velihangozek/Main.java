93592f9de58aceae68bae712375f422ca3145851
Velihan Gözek <velihangozek@hotmail.com>
2025-04-12 03:17:24 +0300

    update method name for first reverse algorithm accordingly

--- Week-2/FirstReverseAlgorithm/src/main/java/org/velihangozek/Main.java
+++ Week-2/FirstReverseAlgorithm/src/main/java/org/velihangozek/Main.java
@@ -4,13 +4,13 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static String FirstReverse(String str) {
+    public static String firstReverse(String str) {
         return new StringBuilder(str).reverse().toString();
     }
 
     public static void main (String[] args) {
         // keep this function call here
         Scanner s = new Scanner(System.in);
-        System.out.print(FirstReverse(s.nextLine()));
+        System.out.print(firstReverse(s.nextLine()));
     }
 }
