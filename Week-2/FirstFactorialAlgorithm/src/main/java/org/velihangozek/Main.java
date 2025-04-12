6d1e8b109466b9ce073049913e4ad398e086b133
Velihan Gözek <velihangozek@hotmail.com>
2025-04-12 03:31:29 +0300

    update method name for first factorial algorithm accordingly

--- Week-2/FirstFactorialAlgorithm/src/main/java/org/velihangozek/Main.java
+++ Week-2/FirstFactorialAlgorithm/src/main/java/org/velihangozek/Main.java
@@ -4,17 +4,17 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static int FirstFactorial(int num) {
+    public static int firstFactorial(int num) {
         if (num == 1) {
             return 1;
         }
-        num *= FirstFactorial(num-1);
+        num *= firstFactorial(num-1);
         return num;
     }
 
     public static void main (String[] args) {
         // keep this function call here
         Scanner s = new Scanner(System.in);
-        System.out.print(FirstFactorial(Integer.parseInt(s.nextLine())));
+        System.out.print(firstFactorial(Integer.parseInt(s.nextLine())));
     }
 }
