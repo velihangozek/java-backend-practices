08174dc435400111eb3fff241d85238f16e396b9
Velihan Gözek <velihangozek@hotmail.com>
2025-05-12 03:02:22 +0300

    update dvdrental-sql-task-4 with best practices

--- Week-6/SQL/dvdrental-sql-task-4/dvdrental-sql-task-4.sql
+++ Week-6/SQL/dvdrental-sql-task-4/dvdrental-sql-task-4.sql
@@ -1,20 +1,26 @@
 SELECT DISTINCT replacement_cost
 FROM film;
 
-SELECT COUNT(DISTINCT replacement_cost)
+SELECT COUNT(DISTINCT replacement_cost) AS distinct_replacement_cost_count
 FROM film;
 
+-- Hint for the below query
 SELECT title, rating
 FROM film
 WHERE title LIKE 'T%'
   AND rating = 'G';
+
+SELECT COUNT(*) AS film_count_T_and_G
+FROM film
+WHERE title LIKE 'T%'
+ AND rating = 'G';
 
 -- Hint for the below query
 SELECT country
 FROM country
 WHERE country LIKE '_____';
 
-SELECT COUNT(country)
+SELECT COUNT(country) AS country_count_length_5
 FROM country
 WHERE country LIKE '_____';
 
@@ -23,6 +29,11 @@ SELECT city
 FROM city
 WHERE city ILIKE '%R';
 
-SELECT COUNT(city)
+SELECT COUNT(*) AS city_count_ending_with_r
 FROM city
 WHERE city ILIKE '%R';
+
+-- Alternative to the above
+SELECT COUNT(*) AS city_count_ending_with_r
+FROM city
+WHERE LOWER(city) LIKE '%r';
