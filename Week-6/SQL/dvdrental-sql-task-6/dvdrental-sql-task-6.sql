08d0bf0fe54b37c388f732cd2e5979e8e54ac515
Velihan Gözek <velihangozek@hotmail.com>
2025-05-12 21:10:52 +0300

    update dvdrental-sql-task-6 with best practices

--- Week-6/SQL/dvdrental-sql-task-6/dvdrental-sql-task-6.sql
+++ Week-6/SQL/dvdrental-sql-task-6/dvdrental-sql-task-6.sql
@@ -1,14 +1,22 @@
-SELECT ROUND(AVG(rental_rate), 3)
+-- 1. Average rental_rate (rounded to 3 decimals)
+SELECT ROUND(AVG(rental_rate), 3) AS avg_rental_rate
 FROM film;
 
-SELECT COUNT(*)
+-- Alternative
+SELECT ROUND(AVG(rental_rate)::numeric, 3) AS avg_rental_rate
+FROM film;
+
+-- 2. Number of films with titles starting with ‘C’
+SELECT COUNT(*) AS films_starting_with_C
 FROM film
 WHERE title LIKE 'C%';
 
-SELECT MAX(length)
+-- 3. Longest film length where rental_rate = 0.99
+SELECT MAX(length) AS longest_length_for_rate_0_99
 FROM film
 WHERE rental_rate = 0.99;
 
-SELECT COUNT(DISTINCT replacement_cost)
+-- 4. Distinct replacement_cost values for films longer than 150 min
+SELECT COUNT(DISTINCT replacement_cost) AS distinct_replacement_costs
 FROM film
 WHERE length > 150;
